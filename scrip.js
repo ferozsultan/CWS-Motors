df374055138a74d2b2495f204ca576cae6a028b4
ferozsultan <ferozsultan10@gmail.com>
2026-05-30 00:26:12 +0430

    CWS motor files

---
+++ scrip.js
@@ -0,0 +1,11 @@
+// alert("Hello World! Welcome to my website, you can find more about me and my work here. Feel free to explore and contact me if you have any questions or opportunities. Enjoy your stay!");
+
+
+const navbarToggel = document.querySelector('.navbarToggel');
+const navLink = document.querySelector('.navLink');
+
+
+navbarToggel.addEventListener('click', () => {
+    navbarToggel.classList.toggle('active');
+    navLink.classList.toggle('active');
+});
