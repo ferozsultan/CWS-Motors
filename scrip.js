a4964b1fc2798344e0fc734178eecd614e355461
ferozsultan <ferozsultan10@gmail.com>
2026-06-02 00:23:17 +0430

    dynamic

--- scrip.js
+++ scrip.js
@@ -11,3 +11,87 @@ navbarToggel.addEventListener('click', () => {
 });
 
 
+let nav = document.getElementById("nav");
+nav.classList("nav");
+
+let navbar_container = document.getElementById("navbar_container");
+navbar_container.classList("navbar-container");
+
+let navbarToggel =document.getElementById("navbarToggel");
+navbarToggel.classList("navbarToggel");
+
+let bar = document.getElementById("bar");
+bar.classList("bar");
+
+let navLink = document.getElementById("navLink");
+navLink.classList("navLink");
+
+let nav_do_img =document.getElementById("nav_do_img");
+nav_do_img.classList("nav-do-img");
+
+// end of navbar
+
+let main_hero_sec = document.getElementById("main_hero_sec");
+main_hero_sec.classList("main_hero_sec");
+
+let btn = document.getElementById("btn");
+btn.classList("btn");
+
+// end of hero section
+
+let sec2 = document.getElementById("sec2");
+sec2.classList("sec2");
+
+let sec2_cards = document.getElementById("sec2_cards"); 
+sec2_cards.classList("sec2_cards");
+
+let card = document.getElementById("card");
+card.classList("card");
+
+let card1 = document.getElementById("card1");
+card1.classList("card1");
+
+// end of section 2
+
+let sec3 = document.getElementById("sec3");
+sec3.classList("sec3");
+
+let sec3_text = document.getElementById("sec3_text");
+sec3_text.classList("sec3_text");
+
+let sec3_main_card = document.getElementById("sec3_main_card");
+sec3_main_card.classList("sec3_main_card");
+
+let sec3_card= document.getElementById("sec3_card");
+sec3_card.classList("sec3_card");
+
+let sec3_card_text= document.getElementById("sec3_card_text");
+sec3_card_text.classList("sec3_card_text");
+
+let mt15= document.getElementById("mt15");
+mt15.classList("mt15");
+
+let gsxr=document.getElementById("gsxr");
+gsxr.classList("gsxr");
+
+let hayabusa=document.getElementById("hayabusa");
+hayabusa.classList("hayabusa");
+
+// end of section 3
+
+let sec4= document.getElementById("sec4");
+sec4.classList("sec4");
+
+let sec4_email= document.getElementById("sec4_email");
+sec4_email.classList("sec4_email");
+
+let sec4_input= document.getElementById("sec4_input");
+sec4_input.classList("sec4_input");
+
+let email =document.getElementById("email"); 
+email.classList("email");
+
+let submit = document.getElementById("submit");
+submit.classList("submit");
+
+// end of secrion 4
