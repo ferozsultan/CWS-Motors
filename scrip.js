58d1e9574d80f4500594c9509e057afee8b40abf
ferozsultan <ferozsultan10@gmail.com>
2026-06-11 11:54:29 +0430

    n

--- scrip.js
+++ scrip.js
@@ -1,4 +1,4 @@
-// alert("Hello World! Welcome to my website, you can find more about me and my work here. Feel free to explore and contact me if you have any questions or opportunities. Enjoy your stay!");
+alert("Hello World! Welcome to my website, you can find more about me and my work here. Feel free to explore and contact me if you have any questions or opportunities. Enjoy your stay!");
 
 
 const navbarToggel = document.querySelector('.navbarToggel');
@@ -10,88 +10,87 @@ navbarToggel.addEventListener('click', () => {
     navLink.classList.toggle('active');
 });
 
-
 let nav = document.getElementById("nav");
-nav.classList("nav");
+nav.className =("nav");
 
 let navbar_container = document.getElementById("navbar_container");
-navbar_container.classList("navbar-container");
+navbar_container.className =("navbar_container");
 
 let navbarToggel =document.getElementById("navbarToggel");
-navbarToggel.classList("navbarToggel");
+navbarToggel.className =("navbarToggel");
 
 let bar = document.getElementById("bar");
-bar.classList("bar");
+bar.className =("bar");
 
 let navLink = document.getElementById("navLink");
-navLink.classList("navLink");
+navLink.className =("navLink");
 
 let nav_do_img =document.getElementById("nav_do_img");
-nav_do_img.classList("nav-do-img");
+nav_do_img.className =("nav-do-img");
 
 // end of navbar
 
 let main_hero_sec = document.getElementById("main_hero_sec");
-main_hero_sec.classList("main_hero_sec");
+main_hero_sec.className =("main_hero_sec");
 
 let btn = document.getElementById("btn");
-btn.classList("btn");
+btn.className =("btn");
 
 // end of hero section
 
 let sec2 = document.getElementById("sec2");
-sec2.classList("sec2");
+sec2.className =("sec2");
 
 let sec2_cards = document.getElementById("sec2_cards"); 
-sec2_cards.classList("sec2_cards");
+sec2_cards.className =("sec2_cards");
 
 let card = document.getElementById("card");
-card.classList("card");
+card.className =("card");
 
 let card1 = document.getElementById("card1");
-card1.classList("card1");
+card1.className =("card1");
 
 // end of section 2
 
 let sec3 = document.getElementById("sec3");
-sec3.classList("sec3");
+sec3.className =("sec3");
 
 let sec3_text = document.getElementById("sec3_text");
-sec3_text.classList("sec3_text");
+sec3_text.className =("sec3_text");
 
 let sec3_main_card = document.getElementById("sec3_main_card");
-sec3_main_card.classList("sec3_main_card");
+sec3_main_card.className =("sec3_main_card");
 
 let sec3_card= document.getElementById("sec3_card");
-sec3_card.classList("sec3_card");
+sec3_card.className =("sec3_card");
 
 let sec3_card_text= document.getElementById("sec3_card_text");
-sec3_card_text.classList("sec3_card_text");
+sec3_card_text.className =("sec3_card_text");
 
 let mt15= document.getElementById("mt15");
-mt15.classList("mt15");
+mt15.className =("mt15");
 
 let gsxr=document.getElementById("gsxr");
-gsxr.classList("gsxr");
+gsxr.className =("gsxr");
 
 let hayabusa=document.getElementById("hayabusa");
-hayabusa.classList("hayabusa");
+hayabusa.className =("hayabusa");
 
 // end of section 3
 
 let sec4= document.getElementById("sec4");
-sec4.classList("sec4");
+sec4.className =("sec4");
 
 let sec4_email= document.getElementById("sec4_email");
-sec4_email.classList("sec4_email");
+sec4_email.className =("sec4_email");
 
 let sec4_input= document.getElementById("sec4_input");
-sec4_input.classList("sec4_input");
+sec4_input.className =("sec4_input");
 
 let email =document.getElementById("email"); 
-email.classList("email");
+email.className =("email");
 
 let submit = document.getElementById("submit");
-submit.classList("submit");
+submit.className =("submit");
 
-// end of secrion 4
+// end of section 4
